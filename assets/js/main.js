4133ffe3b91e12025f77a9690d0792e5cba910e4
kemaltt <ttnckemal@gmail.com>
2022-05-18 15:09:08 +0200

    last commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,3 +177,34 @@ console.log(newArr);
 newArr.shift();
 newArr.shift();
 console.log(newArr);
+
+// ===========================================================================
+////Lev1_12------- Arrays slice()-------------------
+
+let array = [
+    "imageTeilnehmer000supercode.jpg",
+    "imageTeilnehmer001supercode.jpg",
+    "imageTeilnehmer002supercode.jpg",
+    "imageTeilnehmer003supercode.jpg",
+    "imageTeilnehmer004supercode.jpg",
+    "imageTeilnehmer005supercode.jpg",
+    "imageTeilnehmer006supercode.jpg",
+    "imageTeilnehmer007supercode.jpg",
+    "imageTeilnehmer008supercode.jpg",
+    "imageTeilnehmer009supercode.jpg",
+    "imageTeilnehmer010supercode.jpg",
+    "imageTeilnehmer011supercode.jpg",
+    "imageTeilnehmer012supercode.jpg",
+    "imageTeilnehmer013supercode.jpg",
+    "imageTeilnehmer014supercode.jpg",
+    "imageTeilnehmer015supercode.jpg",
+    "imageTeilnehmer016supercode.jpg",
+    "imageTeilnehmer017supercode.jpg",
+    "imageTeilnehmer018supercode.jpg",
+    "imageTeilnehmer019supercode.jpg"
+];
+
+let copyImg1 = array.slice(7, 15);
+console.log(copyImg1);
+let copyImg2 = array.slice(6, 12);
+console.log(copyImg2);
